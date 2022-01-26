bd2be4d95d283140a0f24fa326a67cd6775a5340
Gabera7 <79115925+Gabera7@users.noreply.github.com>
2022-01-26 20:31:38 -0300

    Create email-functions.php

---
+++ custom_wc_mail_conditionally_triggered/custom-emails/email-functions.php
@@ -0,0 +1,12 @@
+<?php 
+add_filter('woocommerce_email_classes', 'custom_tracking_email');
+function custom_tracking_email($email_classes)
+{
+	//* Custom email class
+	$upload_dir = wp_upload_dir();
+
+	require_once($upload_dir['basedir'] . '/custom-emails/class-tracking-number-email.php');
+	$email_classes['WC_Email_Customer_Tracking_Number'] = new WC_Email_Customer_Tracking_Number(); // add to the list of email classes that WooCommerce loads
+	return $email_classes;
+}
+?>
